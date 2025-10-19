2b4192926e93506c0e427403eb23320ba3247982
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2025-10-18 22:33:32 -0400

    initial commit

---
+++ PropHuntMod/PropHuntMod.cs
@@ -0,0 +1,15 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("com.bobbythecatfish.prophunt", "Prop Hunt", "0.1.0")]
+public class PropHuntMod : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        Logger.LogInfo("Prop Hunt loaded and intialized");
+
+        Harmony.CreateAndPatchAll(typeof(PropHuntMod), null);
+    }
+}
---
+++ PropHuntMod/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PropHuntMod")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PropHuntMod")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ea9e15ba-4504-4c36-baa6-8f85bee1da18")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
