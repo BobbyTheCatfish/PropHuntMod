f7a452dd0762728d5e41a377418148a4ecf605ca
BobbyTheCatfish <46359040+BobbyTheCatfish@users.noreply.github.com>
2025-10-20 01:44:47 -0400

    post-testing a bunch of stuff

--- PropHuntMod/PropHuntMod.cs
+++ PropHuntMod/PropHuntMod.cs
@@ -1,31 +1,79 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using GenericVariableExtension;
 using HarmonyLib;
 using HarmonyLib.Tools;
-using SSDebug.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Reflection;
+using UnityEditor;
 using UnityEngine;
-using UnityEngineInternal;
+
+
 
 [BepInPlugin("com.bobbythecatfish.prophunt", "Prop Hunt", "0.1.0")]
 [BepInProcess("Hollow Knight Silksong.exe")]
 public class PropHuntMod : BaseUnityPlugin
 {
+    
     GameObject hornet;
     GameObject cover;
+    Props props = new Props();
+    bool show = true;
+    Vector2 ogHitboxSize;
+    Vector2 currentHitboxSize;
+    BoxCollider2D hitbox;
+
+    private void ToggleHide()
+    {
+        tempLog("Toggling Hornet");
+        setHornet();
+        var render = hornet.GetComponent<MeshRenderer>();
+        render.enabled = !show;
+        show = !show;
+    }
+
     private void Awake()
     {
         tempLog("Prop Hunt Loaded.");
+        props.Initialize();
         Harmony.CreateAndPatchAll(typeof(PropHuntMod), null);
+    }
+
+    private void setHornet()
+    {
+        hornet = GameObject.FindGameObjectWithTag("Player");
+        tempLog(hornet.name);
+        if (hitbox == null)
+        {
+            //hitbox = hornet.GetComponent<BoxCollider2D>();
+            var hitboxObj = GameObject.FindGameObjectWithTag("HeroBox");
+            tempLog(hitboxObj.name);
+            hitbox = hitboxObj.GetComponent<BoxCollider2D>();
+            Console.WriteLine(hitbox.size);
+            ogHitboxSize = hitbox.size;
+        }
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        // For some reason, the game updates the collision size automatically. not sure if theres a better way (or if i even want to do this at all)
+        if (hitbox != null && currentHitboxSize != null && hitbox.size.x != currentHitboxSize.x)
         {
-            hornet = GameObject.FindGameObjectWithTag("Player");
+            hitbox.size = new Vector3(currentHitboxSize.x, hitbox.size.y);
+        }
 
+        // TOGGLE VISIBILITY
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            ToggleHide();
+            return;
+        }
+        else if (Input.GetKeyDown(KeyCode.P))
+        {
+
+            setHornet();
             var position = hornet.transform;
             Console.WriteLine(position.localPosition);
 
@@ -34,8 +82,26 @@ public class PropHuntMod : BaseUnityPlugin
                 GameObject.Destroy(cover);
             }
 
-            var clump = GameObject.Find("moss_clump_set (41)");
-            cover = GameObject.Instantiate(clump, hornet.transform.position, hornet.transform.rotation, hornet.transform);
+            var prop = props.boneBottom.GetRandomElement();
+            var existing = GameObject.Find(prop.name);
+            if (existing == null)
+            {
+                tempLog($"Couldn't get GameObject {prop.name}");
+                return;
+            }
+            cover = GameObject.Instantiate(existing, hornet.transform.position, hornet.transform.rotation, hornet.transform);
+            cover.transform.SetPositionX(cover.transform.position.x + prop.positionOffset.x);
+            cover.transform.SetPositionY(cover.transform.position.y + prop.positionOffset.y);
+
+            var sprite = cover.GetComponent<SpriteRenderer>();
+            if (!sprite)
+            {
+                tempLog($"Couldn't get sprite for {cover.name}");
+                return;
+            }
+            var size = sprite.sprite.rect.size;
+            currentHitboxSize = new Vector3(size.x / 50, hitbox.size.y);
+            hitbox.size = currentHitboxSize;
 
 
             //GameObject[] allGameObjects = Resources.FindObjectsOfTypeAll<GameObject>();
